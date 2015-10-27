78a80de6baf59319a2c9a9f9eae28d7415fb6241
nnam-ibe <nnam.ibe@gmail.com>
2015-10-26 22:39:40 -0400

    adding neccessary files to git

--- app/src/main/java/show/com/tron/MainActivity.java
+++ app/src/main/java/show/com/tron/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
-import android.preference.PreferenceManager;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
